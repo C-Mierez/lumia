9f1193e7a4bfa55062ca1d3d4140b7fce856f5a4
C-Mierez <carlos.mierez20@gmail.com>
2025-02-06 00:06:40 -0300

    🌈 style: Added branding assets and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <div>
-            Home Page
-            <div>
-                <Button variant={"default"}>Test</Button>
+            <div className="flex items-end gap-0.5 m-4">
+                <Image src={"/logo.svg"} height={28} width={28} alt="Logo" />
+                <h1 className="font-bold text-xl tracking-tighter font-brand leading-4">Lumia</h1>
             </div>
         </div>
     );
