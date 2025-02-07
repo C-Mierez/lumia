2872f51155839601951b9cadc0b97e5adb5ee63a
C-Mierez <carlos.mierez20@gmail.com>
2025-02-07 18:23:04 -0300

    🌈 style: Update button and input components for improved colors

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -10,12 +10,12 @@ export default function SearchInput() {
         <form className="flex h-full w-full transition-colors">
             <Input
                 placeholder="Search for videos"
-                className="focus:bg-accent-600/20 border-foreground/50 focus:border-foreground/75 h-9 rounded-l-md rounded-r-none border border-r-0 bg-transparent focus:outline-none focus-visible:ring-0"
+                className="focus:bg-accent-600/20 focus:border-foreground/75 h-9 rounded-l-md rounded-r-none border-r-0 bg-transparent focus:outline-none focus-visible:ring-0"
             />
             <Button
                 type="submit"
                 variant={"ghost"}
-                className="border-foreground/50 rounded-l-none rounded-r-md border focus:outline-none disabled:cursor-default disabled:opacity-50"
+                className="border-input rounded-l-none rounded-r-md border focus:outline-none disabled:cursor-default disabled:opacity-50"
             >
                 <SearchIcon />
             </Button>
