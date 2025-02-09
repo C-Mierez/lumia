e5e1c52c07c64db47fbe29b0d7b9e1433a6e319c
C-Mierez <carlos.mierez20@gmail.com>
2025-02-09 16:34:40 -0300

    🐳 chore: Update ngrok script and modify environment variable validations

--- src/env.js
+++ src/env.js
@@ -21,10 +21,10 @@ export const env = createEnv({
     client: {
         // NEXT_PUBLIC_CLIENTVAR: z.string(),
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-        NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().url(),
-        NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().url(),
-        NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: z.string().url(),
-        NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: z.string().url(),
+        NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
+        NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string(),
+        NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: z.string(),
+        NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: z.string(),
     },
 
     /**
