ba43ea4006c97103ad7a2964a4ff85846637d762
C-Mierez <carlos.mierez20@gmail.com>
2025-02-10 04:41:20 -0300

    ✨ feat: Add range utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+// Declarative iterator
+export function range(n: number) {
+    return Array.from({ length: n }, (_, i) => i);
+}
