6514696ca8782361d7d9e4e29d6b705b5bad5065
C-Mierez <carlos.mierez20@gmail.com>
2025-02-12 18:45:26 -0300

    ✨ feat: Add VideoThumbnail component for displaying video previews with duration

---
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+import SVG from "@components/svg/svg";
+import { formatVideoDuration } from "@lib/utils";
+
+interface VideoThumbnailProps {
+    title: string;
+    imageUrl?: string | null;
+    previewUrl?: string | null;
+    duration: number;
+}
+
+export default function VideoThumbnail({ title, imageUrl, previewUrl, duration }: VideoThumbnailProps) {
+    return (
+        <div className="group relative overflow-hidden">
+            <div className="relative aspect-video w-full overflow-hidden rounded-md">
+                {!!imageUrl && !!previewUrl ? (
+                    <>
+                        <div className="bg-muted repeat-[8] absolute inset-0 animate-pulse" />
+                        <Image alt={title} src={previewUrl} fill className="group-hover:opacity-100" />
+                        <Image
+                            alt={title}
+                            src={imageUrl}
+                            fill
+                            className="transition-opacity duration-200 group-hover:opacity-0"
+                        />
+                    </>
+                ) : (
+                    <SVG.PlaceholderVideo className="size-full" />
+                )}
+            </div>
+            {!!duration && (
+                <div className="bg-background/50 absolute right-0 bottom-0 rounded-tl-md rounded-bl-md p-1 text-xs">
+                    {formatVideoDuration(duration)}
+                </div>
+            )}
+        </div>
+    );
+}
