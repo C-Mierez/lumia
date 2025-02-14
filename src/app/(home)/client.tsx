7eac12f988b6d52f25ad1190c163b894a348a2de
C-Mierez <carlos.mierez20@gmail.com>
2025-02-14 01:22:39 -0300

    🦄 refactor: Removed unused component

--- src/app/(home)/client.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { trpc } from "@/trpc/client";
-
-export default function HomeClient() {
-    const [data] = trpc.categories.getMany.useSuspenseQuery();
-
-    return <>{JSON.stringify(data)}</>;
-}
