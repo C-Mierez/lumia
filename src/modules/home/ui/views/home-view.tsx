2def93c6eecbed610cdac5ca62bcf77507292728
C-Mierez <carlos.mierez20@gmail.com>
2025-02-15 01:56:11 -0300

    🐞 fix: Unnecessary margins

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -6,7 +6,7 @@ interface HomeViewProps {
 
 export default function HomeView({ categoryId }: HomeViewProps) {
     return (
-        <div className="mx-auto flex max-w-[var(--screen-max)] flex-col gap-y-6 px-4 py-2">
+        <div className="flex max-w-[var(--screen-max)] flex-col gap-y-6 px-4 py-2">
             <CategoriesSection categoryId={categoryId} />
         </div>
     );
