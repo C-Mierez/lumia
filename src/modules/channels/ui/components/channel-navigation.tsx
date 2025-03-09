712a79b2c9ff6b6b51e78234ce102741d902b0ac
C-Mierez <carlos.mierez20@gmail.com>
2025-03-09 13:54:31 -0300

    feat: :sparkles: Channel active tab indication

--- src/modules/channels/ui/components/channel-navigation.tsx
+++ src/modules/channels/ui/components/channel-navigation.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { buildSearchQuery, SEARCH_KEY_VALUES } from "@lib/searchParams";
+import { cn } from "@lib/utils";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 interface ChannelNavigationProps {}
 
@@ -26,6 +27,7 @@ const Items = [
 ];
 
 export default function ChannelNavigation({}: ChannelNavigationProps) {
+    const pathname = usePathname();
     const searchParams = useSearchParams();
     const userId = searchParams.get(SEARCH_KEY_VALUES.Home.u) || "";
 
@@ -35,12 +37,18 @@ export default function ChannelNavigation({}: ChannelNavigationProps) {
                 <ul className="flex gap-2">
                     {Items.map((item, index) => {
                         const url = `${item.href}${buildSearchQuery({ u: userId })}`;
-
+                        const isActive = pathname === item.href;
                         return (
-                            <li key={index}>
+                            <li key={index} className="relative">
                                 <Link href={url} className="text-body block size-full px-5 py-2 text-sm font-semibold">
                                     {item.title}
                                 </Link>
+                                <div
+                                    className={cn(
+                                        "bg-accent absolute bottom-0 left-0 h-0 w-full transition-[height]",
+                                        isActive && "h-0.5",
+                                    )}
+                                ></div>
                             </li>
                         );
                     })}
