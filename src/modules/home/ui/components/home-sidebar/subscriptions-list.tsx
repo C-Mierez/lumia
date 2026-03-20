b808821eff499f4d9aecf604850032db8b552ed0
C-Mierez <carlos.mierez20@gmail.com>
2026-03-20 13:23:11 -0300

    feat: Improved UI for subscription list in the menu during first load

--- src/modules/home/ui/components/home-sidebar/subscriptions-list.tsx
+++ src/modules/home/ui/components/home-sidebar/subscriptions-list.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { useTRPC } from "@/trpc/client";
-import { useAuth } from "@clerk/nextjs";
+import { SignUpButton, useAuth } from "@clerk/nextjs";
 import { Avatar, AvatarImage } from "@components/ui/avatar";
 import { Separator } from "@components/ui/separator";
 import {
@@ -18,19 +18,38 @@ import {
     SidebarMenuItem,
 } from "@components/ui/sidebar";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { Loader2Icon } from "lucide-react";
+import { Loader2Icon, Smile } from "lucide-react";
 import { getFullChannelUrl } from "@lib/utils";
+import { Button } from "@components/ui/button";
 
 export default function SubscriptionsList() {
-    const { isSignedIn } = useAuth();
+    const { isLoaded, isSignedIn } = useAuth();
     const pathname = usePathname();
+
+    if (!isLoaded) {
+        return (
+            <>
+                <Separator />
+                <SubscriptionsListSkeleton />
+            </>
+        );
+    }
+
+    if (!isSignedIn) {
+        return (
+            <>
+                <Separator />
+                <SubscriptionsListLoggedOut />
+            </>
+        );
+    }
 
     return (
         <>
             <Separator />
 
             <Suspense fallback={<SubscriptionsListSkeleton />}>
-                {isSignedIn && <SubscriptionsListSuspense pathname={pathname} />}
+                <SubscriptionsListSuspense pathname={pathname} />
             </Suspense>
         </>
     );
@@ -65,6 +84,12 @@ function SubscriptionsListSuspense({ pathname }: SubscriptionsListSuspenseProps)
                             </SidebarMenuButton>
                         </SidebarMenuItem>
                     ))}
+                    {data?.length === 0 && (
+                        <SidebarMenuItem className="flex flex-col items-center gap-2 p-2">
+                            <Smile className="text-muted-foreground size-8 rotate-180" />
+                            <p className="text-muted-foreground text-center text-xs">You have no subscriptions</p>
+                        </SidebarMenuItem>
+                    )}
                 </SidebarMenu>
             </SidebarGroupContent>
         </SidebarGroup>
@@ -72,5 +97,36 @@ function SubscriptionsListSuspense({ pathname }: SubscriptionsListSuspenseProps)
 }
 
 function SubscriptionsListSkeleton() {
-    return <Loader2Icon className="mx-auto mt-4 size-4 animate-spin" />;
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent>
+                <SidebarGroupLabel className="whitespace-nowrap">Subscriptions</SidebarGroupLabel>
+                <SidebarMenu>
+                    <SidebarMenuItem className="flex flex-col items-center gap-2 p-2">
+                        <Loader2Icon className="size-3 animate-spin" />
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
+}
+
+function SubscriptionsListLoggedOut() {
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent>
+                <SidebarGroupLabel className="whitespace-nowrap">Subscriptions</SidebarGroupLabel>
+                <SidebarMenu>
+                    <SidebarMenuItem className="flex flex-col items-center gap-2 p-2">
+                        <div className="text-xs">Log in to subscribe, comment and like videos.</div>
+                        <SignUpButton mode="modal">
+                            <Button variant={"outline"} size={"sm"} className="text-xs">
+                                Sign Up
+                            </Button>
+                        </SignUpButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
 }
